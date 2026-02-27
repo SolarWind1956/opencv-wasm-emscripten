983316149fafb55c88488a8cd812f016f40d8708
Sergej Edgar Heckel <Solar_Wind1956@yahoo.com>
2026-02-27 20:41:28 +0200

    Update build.py

--- scripts/build.py
+++ scripts/build.py
@@ -1,27 +1,17 @@
-import os
-import subprocess
-import sys
+import os, subprocess, sys
 
 def build():
-    print("🚀 Собираем проект через Emscripten Ports...")
-    
+    print("🚀 Собираем ядро WASM...")
     command = [
         "emcc", "src/main.cpp",
         "-o", "index.js",
-        "--bind",                      # Связь C++ и JS
-        "-s", "USE_OPENCV=1",          # МАГИЯ: Emscripten сам скачает OpenCV
+        "--bind", 
         "-s", "ALLOW_MEMORY_GROWTH=1",
         "-O3"
     ]
-
-    print(f"🔨 Выполняем: {' '.join(command)}")
     result = subprocess.run(command)
-    
-    if result.returncode == 0:
-        print("✅ ПОБЕДА! Файлы готовы.")
-    else:
-        print("❌ Ошибка сборки.")
-        sys.exit(1)
+    sys.exit(result.returncode)
 
 if __name__ == "__main__":
     build()
+
