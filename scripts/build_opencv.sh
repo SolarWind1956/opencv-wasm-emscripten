0f444a9ce2f3d61e2377b3879471e82ae75dec33
Sergej Edgar Heckel <Solar_Wind1956@yahoo.com>
2026-02-28 11:09:14 +0200

    Update build_opencv.sh

--- scripts/build_opencv.sh
+++ scripts/build_opencv.sh
@@ -2,9 +2,7 @@
 # 1. Клонируем исходный код OpenCV (версия 4.10.0)
 git clone --depth 1 --branch 4.10.0 https://github.com/opencv/opencv.git
 
-# 2. Переходим в папку со скриптами сборки
-cd opencv
-
-# 3. Запускаем официальный сборщик OpenCV для WebAssembly
-# Это самая долгая часть — она превращает C++ исходники в WASM-библиотеку
-python3 platforms/js/build_js.py build_wasm --build_wasm
+# 2.  
+echo "--- Начинаем сборку с флагом C++17 ---"
+# Мы добавляем переменную CMAKE_CXX_FLAGS, чтобы исправить ошибку
+python3 opencv/platforms/js/build_js.py build_wasm --build_wasm --cmake_option="-DCMAKE_CXX_FLAGS=-std=c++17"
