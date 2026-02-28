0908601d334c3a42b5bc3603bc44277ca88e000a
Sergej Edgar Heckel <Solar_Wind1956@yahoo.com>
2026-02-28 11:27:04 +0200

    Update build_opencv.sh

--- scripts/build_opencv.sh
+++ scripts/build_opencv.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-# 1. Клонируем исходный код OpenCV (версия 4.10.0)
-git clone --depth 1 --branch 4.10.0 https://github.com/opencv/opencv.git
+# 1. Клонируем (если папки нет)
+if [ ! -d "opencv" ]; then
+  git clone --depth 1 --branch 4.10.0 https://github.com/opencv/opencv.git
+fi
 
-# 2.  
-echo "--- Начинаем сборку с флагом C++17 ---"
-# Мы добавляем переменную CMAKE_CXX_FLAGS, чтобы исправить ошибку
-python3 opencv/platforms/js/build_js.py build_wasm --build_wasm --cmake_option="-DCMAKE_CXX_FLAGS=-std=c++17"
+# 2. Сборка с жестким указанием стандарта C++17
+# Мы добавляем OPENCV_FORCE_PYTHON_CTYPES и CMAKE_CXX_STANDARD
+python3 opencv/platforms/js/build_js.py build_wasm --build_wasm \
+  --cmake_option="-DCMAKE_CXX_STANDARD=17" \
+  --cmake_option="-DCMAKE_CXX_FLAGS=-std=c++17" \
+  --cmake_option="-DCMAKE_C_FLAGS=-std=c11"
+
+  
